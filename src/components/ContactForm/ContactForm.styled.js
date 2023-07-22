ba94aa87fc7d6679964333ba60f2d1b01eb23ca6
KRVitalik <karmelitavitalik@gmail.com>
2023-07-22 17:31:45 +0300

    Update ContactForm.styled.js

--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
+  gap:16px;
   `
 
 export const Label = styled.label`
